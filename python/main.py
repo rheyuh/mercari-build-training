40eaf06ac3fa49a45fb02468338b0776482ac4a2
Rhea Iai <rhea@Rheas-MacBook-Pro.local>
2025-03-13 03:09:13 +1100

    docs: test commit for workflow

--- python/main.py
+++ python/main.py
@@ -57,7 +57,7 @@ logger = logging.getLogger("uvicorn")
 # change logger level to display varying levels of logs displayed on console
 logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
-origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
+origins = [os.environ.get("FRONT_URL", "http://0.0.0.0:3000")]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -262,3 +262,5 @@ def hash_image(image):
         image_hash = hashlib.sha256(image_bytes).hexdigest()
         image_hash = image_hash + ".jpg"
     return image_hash
+
+    # test
